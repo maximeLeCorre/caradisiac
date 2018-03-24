a4d2eac6e84e1e986ada2db82d4350d79e91b2f4
unknown <maxime.lecorre96@gmail.com>
2018-03-24 22:18:48 +0100

    First commit

---
+++ insertionElasticsearch.js
@@ -0,0 +1,25 @@
+var elasticsearch = require('elasticsearch');
+var fs = require("fs");
+
+//Initialize elasticsearch connexion
+var elasticsearchClient = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+function insertionElasticSearch(){
+	
+	//Load the json file to insert
+    var json = JSON.parse(fs.readFileSync("./car.json","UTF8"));
+	
+    var request = {body:[]};
+	
+	//Foreach item in the car.json we add it to the bulk request
+    for(var i = 0; i < json.length; i++){    
+        request.body.push({ index:  { _index: 'caradisiac', _type: 'car', _id: i } });
+        request.body.push({  doc : json[i]} );
+    }
+    elasticsearchClient.bulk(request);
+}
+
+insertionElasticSearch();
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var elasticsearch = require('elasticsearch');
+var fs = require("fs");
+
+app.get('/', function(req, res) {
+	res.send('Hello - Use /populate/{your json to insert} Or /cars to get 10 cars with the higher car boot volume');
+});
+
+app.listen(9292);
+console.log('Listening on port 9292...');
+
+app.get('/populate/:car', function(req, res) {
+	
+	//Open a connexion to elasticSearch
+	var elasticsearchClient = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+	});
+	
+	console.log(req.params.car)
+	
+	body = req.params.car;
+	
+	//Insert the data
+	elasticsearchClient.index({  
+		index: 'caradisiac',
+		type: 'car',
+		body
+	},function(err,resp,status) {
+		console.log(resp);
+	});
+	
+	//Was to validate that the data is well inserted
+	/*
+	elasticsearchClient.count({
+		index: 'caradisiac'
+	}, function (error, response) {
+		var count = response.count;
+		console.log(count);
+	});*/
+});
+
+
+app.get('/cars', function(req, res) {
+
+
+   //Open a connexion to elasticSearch
+	var elasticsearchClient = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+	});
+
+	//10 first cars with the higher car boot volume 
+	elasticsearchClient.search({
+		index: 'caradisiac',
+		type: 'car',
+		body: {
+        "from" : 0, "size" : 10, 
+      	"query": {
+      		"bool": {
+				"must": {
+					"exists": {
+						"field": "volume"
+					}
+				}
+			} 
+      	},
+          "sort": [
+                { "volume":   { "order": "desc" }}
+          	]
+      }
+	}, function (error, response) {
+		res.send(response.hits);
+	});
+
+
+});
